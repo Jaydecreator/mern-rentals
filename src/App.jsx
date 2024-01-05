a2a8fab56a6346d18149a1dc209e4a3ffa26693d
Njabulo Alx <ndlovunjabulo17@gmail.com>
2024-01-05 16:32:27 +0200

    first template and css and tailwind

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function App() {
+  return (
+    // <h1 className='text-red-600'> 
+    //   iApp must be large
+    // </h1>
+
+    <h1 className='text-red-500 font-bold font-size'>work</h1>
+  )
+}
